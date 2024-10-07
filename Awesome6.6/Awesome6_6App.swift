6eb6ceeb153f5a8fa49dd1090c8544e51478df02
Anthony Perez <aperez327@gmail.com>
2024-10-07 18:32:28 -0400

    Initial Commit

---
+++ Awesome6.6/Awesome6_6App.swift
@@ -0,0 +1,17 @@
+//
+//  Awesome6_6App.swift
+//  Awesome6.6
+//
+//  Created by Anthony Perez on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Awesome6_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
